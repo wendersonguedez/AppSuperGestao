b252ea10321debf9f1ab36252ba9f41cf0c3d8d7
wenderson guedes <wendersonguedes6@gmail.com>
2022-02-20 17:35:47 -0300

    Nome da rota principal foi alterada

--- resources/views/site/layouts/_partials/topo.blade.php
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -6,7 +6,7 @@
 
     <div class="menu">
         <ul>
-            <li><a href="{{ route('site.index') }}">Home</a></li>
+            <li><a href="{{ route('site.principal') }}">Home</a></li>
             <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
             <li><a href="{{ route('site.contato') }}">Contato</a></li>
         </ul>
