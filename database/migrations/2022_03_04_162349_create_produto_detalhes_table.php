4be1f4494777b6dcf839c0392e10baf09f6f27f7
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-04 13:58:58 -0300

    Criando relacionamento 1:1 entre tabelas

---
+++ database/migrations/2022_03_04_162349_create_produto_detalhes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id', );
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+
+            /* relacionando o campo 'produto_id' da tabela 'produto_detalhes' ao campo 'id' da tabela 'produtos */
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            
+            /* garantindo que o relacionamento é 1:1, ou seja, o campo 'produto_id' tenha valores repetidos */
+            $table->unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
