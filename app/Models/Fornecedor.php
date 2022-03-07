10d26235d44a023a4c0c2ceafeb071fad70dee16
wenderson guedes <wendersonguedes6@gmail.com>
2022-03-06 21:33:19 -0300

    Utilizando o atributo $fillable

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -10,5 +10,8 @@ class Fornecedor extends Model
     /* definindo o nome da tabela que os dados serão enviados. o atributo $table vai sobrepor o nome que foi definida automaticamente pelo Eloquent */
     protected $table = 'fornecedores';
 
+    /* indicando quais campos podem ser atribuidos em massa, que seria a passagem de vários valores de uma vez só para a inserção no banco ao executa o metodo create. */
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
+
     use HasFactory;
 }
